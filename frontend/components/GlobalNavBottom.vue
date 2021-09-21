80ed3ffb1b6a0fcb40f36343165cf69e9beeac36
Ry0xi <ryoki0525tnk@gmail.com>
2021-09-21 23:32:19 +0900

    [f-add]ナビゲーションをコンポーネント化。

---
+++ frontend/components/GlobalNavBottom.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-bottom-navigation
+    app
+    color="secondary"
+  >
+    <v-btn v-for="menu in menus" :key="menu.title" :to="menu.to">
+      <span>{{ menu.title }}</span>
+      <v-icon>{{ menu.icon }}</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      menus: [
+        {
+          icon: 'mdi-home',
+          title: 'ホーム',
+          to: '/'
+        },
+        {
+          icon: 'mdi-format-list-bulleted',
+          title: 'タスク',
+          to: '/tasks'
+        },
+        {
+          icon: 'mdi-calendar-month',
+          title: 'カレンダー',
+          to: '/calendar'
+        },
+        {
+          icon: 'mdi-cog',
+          title: '設定',
+          to: '/settings'
+        }
+      ],
+    }
+  }
+}
+</script>
