32ffa08f34fddaf0f9368c645127a412139af5ae
Ry0xi <ryoki0525tnk@gmail.com>
2021-09-22 18:53:56 +0900

    [f-add]共通レイアウトに各ページのタイトルを表示するように変更。

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <p>Home</p>
-    </v-col>
-  </v-row>
+  <p>Home</p>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      header: {
+        title: 'ホーム'
+      }
+    }
+  },
+  mounted() {
+    this.updateHeader()
+  },
+  methods: {
+    updateHeader() {
+      // タイトルとして使いたい情報を渡す
+      this.$nuxt.$emit('updateHeader', this.header.title)
+    }
+  }
+}
+</script>
--- frontend/pages/tasks.vue
+++ frontend/pages/tasks.vue
@@ -1,3 +1,24 @@
 <template>
   <p>tasks</p>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      header: {
+        title: 'タスク一覧'
+      }
+    }
+  },
+  mounted() {
+    this.updateHeader()
+  },
+  methods: {
+    updateHeader() {
+      // タイトルとして使いたい情報を渡す
+      this.$nuxt.$emit('updateHeader', this.header.title)
+    }
+  }
+}
+</script>
