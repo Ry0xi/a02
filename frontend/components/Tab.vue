0309bf2cdb5ded3cf9ac728a026e32ee6a4796df
2320haru-code <2320haru.code@gmail.com>
2021-09-22 16:06:12 +0900

    [f-add]タブコンポーネントの追加

---
+++ frontend/components/Tab.vue
@@ -0,0 +1,85 @@
+<template>
+  <div :style="styles" class="tab">
+    <div @click="change(1)" class="tab_item" :class="{ active: setTab === 1 }">
+      {{ leftName }}
+    </div>
+    <div @click="change(2)" class="tab_item" :class="{ active: setTab === 2 }">
+      {{ rightName }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'tab',
+    event: 'change',
+  },
+  props: {
+    tab: Number,
+
+    leftName: {
+      type: String,
+      default: 'tab1',
+    },
+    rightName: {
+      type: String,
+      default: 'tab2',
+    },
+    width: {
+      type: String,
+      default: '300px',
+    },
+  },
+  computed: {
+    setTab: {
+      get() {
+        return this.tab
+      },
+      set(value) {
+        return this.$emit('change', value)
+      },
+    },
+    styles() {
+      return {
+        '--tab-width': this.width,
+      }
+    },
+  },
+  methods: {
+    change(num) {
+      this.setTab = num
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.tab {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: var(--tab-width);
+  height: 50px;
+  margin: 0 auto;
+  background-color: #f5f5f5;
+  border-radius: 20px;
+  cursor: pointer;
+  &_item {
+    width: 50%;
+    height: 100%;
+    color: #aaa;
+    font-weight: bold;
+    border-radius: 20px;
+    text-align: center;
+    line-height: 50px;
+    transition: 0.2s;
+
+    &.active {
+      color: #fff;
+      background-color: #f8c852;
+      transition: 0.2s;
+    }
+  }
+}
+</style>
