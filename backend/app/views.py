11b48fb01876616c0733a3edbd2241d9bd221ad3
tokiii888 <takaya.4208@gmail.com>
2021-09-23 15:18:37 +0900

    [b-add] ViewSetの定義

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import django_filters
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from .models import User, Task, Category, History
+from .serializer import UserSerializer, TaskSerializer, CategorySerializer, HistorySerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+  queryset = Category.objects.all()
+  serializer_class = CategorySerializer
+
+
+class HistoryViewSet(viewsets.ModelViewSet):
+  queryset = History.objects.all()
+  serializer_class = HistorySerializer
