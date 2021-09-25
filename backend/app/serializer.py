746800fc94eb425cfe7316daf081e7b2f00a955e
tokiii888 <takaya.4208@gmail.com>
2021-09-25 11:40:21 +0900

    [b-add] serializerに全てのフィールドを追加

--- backend/app/serializer.py
+++ backend/app/serializer.py
@@ -5,19 +5,19 @@ from .models import User, Task, Category, History
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields =('user_name', 'email_address', 'token', 'is_notification')
+    fields =('user_id', 'user_name', 'email_address', 'token', 'task_count', 'is_notification')
 
 class TaskSerializer(serializers.ModelSerializer):
   class Meta:
     model = Task
-    fields =('title', 'detail', 'url', 'category_id')
+    fields =('task_id', 'title', 'detail', 'url', 'created_at', 'priority','next_display_date','display_times','consecutive_times','is_update','user_id', 'category_id')
 
 class CategorySerializer(serializers.ModelSerializer):
   class Meta:
     model = Category
-    fields =('category_name', 'color_code')
+    fields =('category_id', 'category_name', 'color_code', 'user_id')
 
 class HistorySerializer(serializers.ModelSerializer):
   class Meta:
     model = History
-    fields =('date', 'feedback', 'number_of_tasks', 'execution_tasks', 'task_id')
+    fields =('history_id', 'date', 'feedback', 'number_of_tasks', 'execution_tasks', 'user_id', 'task_id')
