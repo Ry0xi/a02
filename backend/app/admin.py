e4726881f4dc4eb29dd9be2ad1f0e4a0339729e3
tokiii888 <takaya.4208@gmail.com>
2021-09-25 15:17:34 +0900

    [b-add] admin.pyにモデルを追加

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Task, Category, History
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Task)
+admin.site.register(Category)
+admin.site.register(History)
