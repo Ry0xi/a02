b07ae1fe8b060336e23d4f3a9f3cde9abd608841
tokiii888 <takaya.4208@gmail.com>
2021-10-04 11:26:59 +0900

    月ごと日ごとのタスクを取り出すfilterの実装

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import UserViewSet, TaskViewSet, CategoryViewSet, HistoryViewSet, ManageUserView
+from .views import UserViewSet, TaskViewSet, CategoryViewSet, HistoryViewSet, ManageUserView, TaskDailyAPIView, TaskMonthlyAPIView
 
 router = routers.DefaultRouter()
 router.register('user', UserViewSet)
@@ -12,4 +12,6 @@ urlpatterns = [
     path('myself/',ManageUserView.as_view( ), name='myself'),
     #ユーザ名とパスワードをPOSTするとトークンを返す。
     path('',include(router.urls)),
+    path('task/<int:year>/<int:month>/<int:day>/', TaskDailyAPIView.as_view(),name='daily'),
+    path('task/<int:year>/<int:month>/', TaskMonthlyAPIView.as_view(),name='monthly'),
 ]
