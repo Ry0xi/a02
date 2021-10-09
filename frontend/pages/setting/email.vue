c26aeafdee4c09fc79a599c91cf55b2bd2af0b54
2320haru-code <2320haru.code@gmail.com>
2021-10-09 17:28:55 +0900

    [f-add]メールアドレス変更ページの追加

---
+++ frontend/pages/setting/email.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <Loading :loading="loading" />
+    <v-form ref="EmailForm">
+      <UserFormPassword
+        v-model="user.password"
+        noIcon
+        noValidation
+        label="パスワード"
+      />
+
+      <UserFormEmail v-model="user.email" noIcon label="新しいメールアドレス" />
+
+      <ErrorMessage :message="message" />
+
+      <Btn @click="updatePassword">メールアドレスを変更する</Btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'setting',
+  data() {
+    return {
+      header: {
+        title: 'メールアドレスの設定',
+        backUrl: '/setting',
+      },
+      loading: false,
+      user: {
+        password: '',
+        email: '',
+      },
+      message: null,
+    }
+  },
+  mounted() {
+    this.updateHeader()
+  },
+  methods: {
+    updateHeader() {
+      // タイトルとして使いたい情報を渡す
+      this.$nuxt.$emit('updateHeader', this.header)
+    },
+    // メールアドレスの更新
+    updatePassword() {
+      this.messages = []
+      if (this.$refs.EmailForm.validate()) {
+        console.log('送信')
+        this.$axios
+          .put('/api/setting/reset_email', this.user)
+          .then(() => {
+            this.$router.push('/setting')
+            console.log('更新')
+          })
+          .catch((error) => {
+            this.message = '更新できませんでした'
+          })
+      }
+    },
+  },
+}
+</script>
