5998bd3e88d74d657830be5b5a312852ec6b4b7e
2320haru-code <2320haru.code@gmail.com>
2021-10-09 17:28:43 +0900

    [f-add]パスワード変更ページの追加

---
+++ frontend/pages/setting/password.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <v-form ref="passwordForm">
+      <UserFormPassword
+        v-model="user.password"
+        noIcon
+        noValidation
+        label="現在のパスワード"
+      />
+
+      <UserFormPassword
+        v-model="user.newpassword"
+        noIcon
+        label="新しいパスワード"
+      />
+
+      <ErrorMessage :message="message" />
+
+      <Btn @click="updatePassword">パスワードを変更する</Btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'setting',
+  data() {
+    return {
+      header: {
+        title: 'パスワードの設定',
+        backUrl: '/setting',
+      },
+      user: {
+        password: '',
+        newpassword: '',
+      },
+      message: null,
+    }
+  },
+  mounted() {
+    this.updateHeader()
+  },
+  methods: {
+    updateHeader() {
+      // タイトルとして使いたい情報を渡す
+      this.$nuxt.$emit('updateHeader', this.header)
+    },
+    // パスワードの更新
+    updatePassword() {
+      this.messages = []
+      if (this.$refs.passwordForm.validate()) {
+        console.log('送信')
+        this.$axios
+          .put('/api/setting/reset_password', this.user)
+          .then(() => {
+            console.log('更新')
+            this.$router.push('/setting')
+          })
+          .catch((error) => {
+            this.message = '更新できませんでした'
+          })
+      }
+    },
+  },
+}
+</script>
