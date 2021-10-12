f4ece02ce867587cd2ece8bfea981625a765de69
2320haru-code <2320haru.code@gmail.com>
2021-10-12 15:38:35 +0900

    [f-fix]Dexie.jsのカラムを修正

--- frontend/plugins/dexie.js
+++ frontend/plugins/dexie.js
@@ -5,9 +5,11 @@ const db = new Dexie('db')
 
 // テーブルの作成
 db.version(1).stores({
-  user: 'id, user_name',
-  task: 'id, title, detail, url, priority, next_display_date, display_times, consecutive_times, is_update, category_id',
-  category: 'id, category_name, color_code',
+  user: 'id, name',
+  task: 'id, title, detail, url, priority, next_display_date, display_times, consecutive_times, is_update, categories_id',
+  category: 'id, name, color',
+  offline_task: 'id, type',
+  offline_category: 'id, type',
 })
 
 export default ({}, inject) => {
