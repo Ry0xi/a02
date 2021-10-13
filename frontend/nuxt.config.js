e0a8eae7ea925dd1d6eb0d672657b5b00bff2984
2320haru-code <2320haru.code@gmail.com>
2021-10-13 18:37:20 +0900

    [f-add]baseURLを追加

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -41,7 +41,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: 'http://localhost:8000/',
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
