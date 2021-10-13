afffaa9130234b6feba3ecdb58bd04b83b0669eb
Ry0xi <ryoki0525tnk@gmail.com>
2021-10-13 22:03:30 +0900

    [f-add]テストデータを追加。

--- frontend/plugins/dexie.js
+++ frontend/plugins/dexie.js
@@ -12,6 +12,23 @@ db.version(1).stores({
   offline_category: '&id, type',
 })
 
+const testTasks = [
+  {'id': '1001', 'name': 'タスク１', 'date': '2021-02-27', 'detail': '教材Aのp23~36を学んで、問題集p41~44を解く。', 'categories': ['0001','0002'], 'isDone': true},
+  {'id': '1002', 'name': 'タスク２未完了', 'date': '2021-02-27', 'detail': '', 'categories': ['0001'], 'isDone': false},
+  {'id': '1003', 'name': 'タスク３タスク３未完了', 'date': '2021-02-27', 'detail': '', 'categories': ['0001','0002'], 'isDone': false},
+  {'id': '1004', 'name': 'タスク４タスク４タスク４タスク４', 'date': '2021-02-27', 'detail': '', 'categories': ['0001','0002', '0003'], 'isDone': true},
+  {'id': '1005', 'name': 'タスク５タスク５タスク５タスク５タスク５タスク５タスク５未完了', 'date': '2021-02-27', 'detail': '', 'categories': ['0001','0002','0003'], 'isDone': false},
+  {'id': '1006', 'name': 'タスク６タスク６タスク６タスク６タスク６タスク６タスク６タスク６タスク６タスク６タスク６', 'date': '2021-02-27', 'detail': '', 'categories': ['0001','0002'], 'isDone': true},
+];
+
+const testCategoryData = [
+  {'id': '0001', 'name': 'カテゴリ1カテゴリ1', 'color': '#FFC1C1'},
+  {'id': '0002', 'name': 'カテゴリ2', 'color': '#FF9090'},
+  {'id': '0003', 'name': 'カテゴリ3', 'color': '#D2BBF7'}
+];
+
 export default ({}, inject) => {
-  inject('db', db)
+  inject('db', db);
+  inject('testTasks', testTasks);
+  inject('testCategoryData', testCategoryData);
 }
