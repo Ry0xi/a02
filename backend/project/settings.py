052b10af069c87ea6c17b2e5991fc301d4d3895c
tokiii888 <takaya.4208@gmail.com>
2021-10-13 18:36:22 +0900

    [b-add]corsで3000番ポート許可

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -134,3 +134,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+     'http://127.0.0.1:3000'
+]
