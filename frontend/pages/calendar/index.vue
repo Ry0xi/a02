585f969644347fe2e126693a8d513e5171e6dfd4
2320haru-code <2320haru.code@gmail.com>
2021-10-14 22:51:13 +0900

    [f-fix]カレンダーページの修正
    indexed DBに保存できるように修正

--- frontend/pages/calendar/index.vue
+++ frontend/pages/calendar/index.vue
@@ -30,11 +30,15 @@
       <div class="taskBox">
         <div>
           <draggable
-            v-model="tasks"
+            v-model="taskItems"
             class="taskBox_items"
             :group="{ name: 'ITEMS', pull: 'clone', put: false }"
           >
-            <div v-for="task in tasks" :key="task.id" class="taskItem">
+            <div
+              v-for="(task, index) in taskItems"
+              :key="index"
+              class="taskItem"
+            >
               {{ task.name }}
             </div>
           </draggable>
@@ -45,10 +49,14 @@
       v-model="dialog"
       :taskName="task.name"
       :taskDate="task.date"
-      :isDone="false"
-      :categoryData="{}"
-      :tasks="[]"
+      :categoryData="categoryData"
+      @task:created="addTaskData($event)"
+      @category:updated="updateCategoryData($event)"
+      @category:created="addCategoryData($event)"
     />
+    <v-snackbar v-model="snackbar">
+      今日以前にタスクは追加できません。
+    </v-snackbar>
   </div>
 </template>
 
@@ -71,14 +79,10 @@ export default {
         date: '',
       },
       setItem: [],
-      tasks: [
-        { id: 1, name: 'task1' },
-        { id: 2, name: 'task2' },
-        { id: 3, name: 'task3' },
-        { id: 4, name: 'task4' },
-        { id: 5, name: 'task5' },
-        { id: 6, name: 'task6' },
-      ],
+      taskItems: [],
+      tasks: [],
+      categoryData: null,
+      snackbar: false,
     }
   },
   computed: {
@@ -92,18 +96,148 @@ export default {
   },
   mounted() {
     this.updateHeader()
+    this.getCategoryDataFromDB()
+    this.getTasksFromDB()
   },
   methods: {
+    async getTasksFromDB() {
+      console.log('getTasksFromDB')
+
+      await this.$db.task
+        .toArray()
+        .then((tasks) => {
+          console.log('tasks >> 取得成功')
+          this.taskItems = tasks.filter(
+            (element, index, self) =>
+              self.findIndex((e) => e.name === element.name) === index
+          )
+          this.tasks = tasks
+        })
+        .catch((e) => {
+          console.log('tasks >> 取得失敗')
+          console.log(e.message)
+        })
+    },
+    async getCategoryDataFromDB() {
+      console.log('getCategoryDataFromDB')
+
+      // カテゴリを取得
+      await this.$db.category
+        .toArray()
+        .then((categoryData) => {
+          console.log('categoryData >> 取得成功')
+
+          // データを整形して保持
+          let newData = {}
+          categoryData.forEach((category) => {
+            newData[category.id] = {
+              name: category.name,
+              color: category.color,
+            }
+          })
+          this.categoryData = newData
+        })
+        .catch((e) => {
+          console.log('categoryData >> 取得失敗')
+          console.log(e.message)
+        })
+    },
     updateHeader() {
       // タイトルとして使いたい情報を渡す
       this.$nuxt.$emit('updateHeader', this.header.title)
     },
+    async addTaskData(newTaskData) {
+      // サーバーに追加する
+      // this.$axios.post('/api/task/', newTaskData).then((response) => {
+
+      // })
+
+      // 【テスト】
+      const newId = this.makeTmpId('task')
+      newTaskData.id = newId
+
+      // IDBに登録する
+      this.$db.task.add(newTaskData).then(() => {
+        console.log('タスク追加 >> 成功')
+        this.getTasksFromDB()
+      })
+    },
+    deleteTaskData(taskId) {
+      // IDBから削除
+      this.$db.task.delete(taskId).then(() => {
+        console.log('task deleted.')
+        // タスクデータの再読み込み
+        this.getTasksFromDB()
+      })
+    },
+    updateTaskData(updatedData) {
+      const taskId = updatedData.id
+      delete updatedData.id
+
+      this.$db.task.update(taskId, updatedData).then(() => {
+        console.log('task updated.')
+        // タスクデータの再読み込み
+        this.getTasksFromDB()
+      })
+    },
+    updateCategoryData(updatedCategoryData) {
+      // カテゴリデータを更新
+      const targetId = updatedCategoryData.id
+      delete updatedCategoryData.id
+
+      this.$db.category.update(targetId, updatedCategoryData).then(() => {
+        console.log('category updated.')
+        // カテゴリデータの再読み込み
+        this.getCategoryDataFromDB()
+      })
+    },
+    addCategoryData(newCategoryData) {
+      // カテゴリデータを追加
+      // 【テスト】
+      const newId = this.makeTmpId('category')
+      newCategoryData.id = newId
+
+      this.$db.category.add(newCategoryData).then(() => {
+        console.log('category created.')
+        // カテゴリデータの再読み込み
+        this.getCategoryDataFromDB()
+      })
+    },
+    makeTmpId(type) {
+      // 【追加系api通信が新規IDを返すようになるまで使用】仮の新規IDを生成
+      let existIds = []
+      if (type == 'category') {
+        existIds = Object.keys(this.categoryData).map((id) => Number(id))
+      } else if (type == 'task') {
+        existIds = this.tasks.map((task) => task.id)
+      } else {
+        return
+      }
+
+      // 存在しているIDの最大値＋1
+      let maxId = null
+      if (existIds[0]) {
+        maxId = existIds.reduce((a, b) => {
+          return Math.max(a, b)
+        })
+        return maxId + 1
+      } else {
+        return 1
+      }
+    },
     // 追加時の処理
     onAdd(date) {
-      this.task.name = this.setItem[0].name
-      this.task.date = date
-      this.dialog = true
-      this.setItem = []
+      const now = new Date()
+      now.setDate(now.getDate() - 1)
+      const dt = new Date(date)
+      if (dt < now) {
+        this.snackbar = true
+      } else {
+        this.task.name = this.setItem[0].name
+        this.task.date = date
+        this.dialog = true
+        this.setItem = []
+      }
     },
   },
 }
