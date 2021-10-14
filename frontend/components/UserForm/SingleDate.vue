fc9edf6807b3c389cf58fb2334f1494e5c281a18
2320haru-code <2320haru.code@gmail.com>
2021-10-14 23:31:13 +0900

    [f-fix]表示日を過去にできないように修正

--- frontend/components/UserForm/SingleDate.vue
+++ frontend/components/UserForm/SingleDate.vue
@@ -4,94 +4,87 @@ v-model:      日付
 placeholder:  入力フォームに表示するプレイスホルダー
 -->
 <template>
-  <v-dialog
-    ref="dialog"
-    v-model="dialog"
-    persistent
-    width="290px"
-  >
-    <template v-slot:activator="{ on, attrs }">
-      <v-text-field
-        :value="newDate ? newDate : date"
-        single-line
-        readonly
-        outlined
-        append-icon="mdi-calendar-month"
-        v-bind="attrs"
-        v-on="on"
-        :placeholder="placeholder"
-        class="mt-1"
-      ></v-text-field>
-    </template>
-    <v-date-picker
-      v-model="inputDate"
-      scrollable
-      locale="jp-ja"
-      :day-format="date => new Date(date).getDate()"
-      color="primary"
-    >
-      <v-spacer></v-spacer>
-      <v-btn
-        text
+  <div>
+    <v-dialog v-model="dialog" persistent width="300px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-text-field
+          v-model="date"
+          single-line
+          readonly
+          outlined
+          append-icon="mdi-calendar-month"
+          v-bind="attrs"
+          v-on="on"
+          @click="edit"
+          :placeholder="placeholder"
+          class="mt-1"
+        ></v-text-field>
+        <v-snackbar v-model="snackbar"> 表示日は過去にできません </v-snackbar>
+      </template>
+      <v-date-picker
+        v-model="editDate"
+        scrollable
+        locale="jp-ja"
+        :day-format="(date) => new Date(date).getDate()"
         color="primary"
-        @click="closeDialog"
       >
-        Cancel
-      </v-btn>
-      <v-btn
-        text
-        color="primary"
-        @click="sendNewDate(), closeDialog()"
-      >
-        OK
-      </v-btn>
-    </v-date-picker>
-  </v-dialog>
+        <v-spacer></v-spacer>
+        <v-btn text color="primary" @click="close"> キャンセル </v-btn>
+        <v-btn depressed color="primary" @click="save"> OK </v-btn>
+      </v-date-picker>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
 export default {
   model: {
     prop: 'date',
-    event: 'input'
+    event: 'update',
   },
   props: {
-    date: String,
-    placeholder: String
+    date: {
+      type: String,
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
   },
   data() {
     return {
       dialog: false,
-      newDate: null
+      editDate: null,
+      snackbar: false,
     }
   },
   computed: {
     inputDate: {
       get() {
-        return this.newDate ? this.newDate : this.date
+        return this.date
       },
-      set(value) {
-        this.newDate = value
-      }
-    }
+    },
   },
   methods: {
-    closeDialog: function() {
-      this.refreshInputDate()
+    edit() {
+      this.editDate = this.date
+    },
+    close() {
       this.dialog = false
     },
-    refreshInputDate: function() {
-      this.newDate = null
-    },
-    sendNewDate: function() {
-      if (this.newDate && this.newDate != this.date) {
-        this.$emit('input', this.newDate)
+    save() {
+      const now = new Date()
+      now.setDate(now.getDate() - 1)
+      const dt = new Date(this.editDate)
+      if (dt < now) {
+        this.dialog = false
+        this.snackbar = true
+      } else {
+        this.$emit('update', this.editDate)
+        this.dialog = false
       }
-    }
-  }
+    },
+  },
 }
 </script>
-
-<style>
-
-</style>
