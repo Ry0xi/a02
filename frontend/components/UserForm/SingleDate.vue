6acf33e5318dad56d9e9b530110b3ce80814a535
2320haru-code <2320haru.code@gmail.com>
2021-10-15 20:20:52 +0900

    [f-fix]選択画面で過去を選択できないように修正

--- frontend/components/UserForm/SingleDate.vue
+++ frontend/components/UserForm/SingleDate.vue
@@ -19,12 +19,12 @@ placeholder:  入力フォームに表示するプレイスホルダー
           :placeholder="placeholder"
           class="mt-1"
         ></v-text-field>
-        <v-snackbar v-model="snackbar"> 表示日は過去にできません </v-snackbar>
       </template>
       <v-date-picker
         v-model="editDate"
         scrollable
         locale="jp-ja"
+        :min="new Date().toISOString().substr(0, 10)"
         :day-format="(date) => new Date(date).getDate()"
         color="primary"
       >
@@ -56,7 +56,6 @@ export default {
     return {
       dialog: false,
       editDate: null,
-      snackbar: false,
     }
   },
   computed: {
@@ -74,16 +73,8 @@ export default {
       this.dialog = false
     },
     save() {
-      const now = new Date()
-      now.setDate(now.getDate() - 1)
-      const dt = new Date(this.editDate)
-      if (dt < now) {
-        this.dialog = false
-        this.snackbar = true
-      } else {
-        this.$emit('update', this.editDate)
-        this.dialog = false
-      }
+      this.$emit('update', this.editDate)
+      this.dialog = false
     },
   },
 }
