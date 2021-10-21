d1236aee8ef263cb756c300424be622a96f5ea31
2320haru-code <2320haru.code@gmail.com>
2021-10-21 14:11:24 +0900

    [f-add]認証エラー処理の追加

---
+++ frontend/pages/401.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-card class="px-6 py-12 login-card" outlined>
+      <div>認証情報が正しくありません。</div>
+      <div>再度ログインしてください。</div>
+      <Btn class="mt-6" @click="toLogin"> ログイン画面へ </Btn>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'login',
+  auth: false,
+  methods: {
+    toLogin() {
+      this.$router.push('/login')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.login-card {
+  max-width: 500px;
+  margin: 0 auto;
+}
+</style>
