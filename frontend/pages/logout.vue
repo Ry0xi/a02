75121559761532ac3a6889b2a2856426c151e422
2320haru-code <2320haru.code@gmail.com>
2021-10-21 14:10:17 +0900

    [f-add]ログアウト処理の追加

---
+++ frontend/pages/logout.vue
@@ -0,0 +1,15 @@
+<template></template>
+<script>
+export default {
+  mounted() {
+    this.logout()
+  },
+  methods: {
+    logout() {
+      this.$auth.logout()
+      this.$axios.post('/api/auth/logout')
+      this.$router.push('/')
+    },
+  },
+}
+</script>
