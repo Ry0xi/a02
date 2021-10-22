778c560658e8fbd915e04ddf532d92675b28af82
tokiii888 <takaya.4208@gmail.com>
2021-10-23 00:23:29 +0900

    [b-add]自動更新の処理

--- backend/app/apps.py
+++ backend/app/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+# new =>
+    def ready(self):
+        """
+        This function is called when startup.
+        """
+        from .task_over import start # <= さっき作った start関数をインポート
+        start()
