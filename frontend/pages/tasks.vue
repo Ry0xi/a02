a1a1412ef01af23b748f66de26b1e77174137b05
Ry0xi <ryoki0525tnk@gmail.com>
2021-10-28 22:27:11 +0900

    [f-fix]カテゴリの新規作成処理をVuexActionから参照するように変更。

--- frontend/pages/tasks.vue
+++ frontend/pages/tasks.vue
@@ -180,88 +180,9 @@ export default {
       .then(() => this.$store.dispatch('replaceCategoryStateWithCategoryOnIDBCategory'))
     },
     addCategoryData(newCategoryData) {
-      // カテゴリデータを追加
-      // サーバーに送信
-      const sendData = {
-        'category_name': newCategoryData.name,
-        'color_code': newCategoryData.color,
-      }
-
-      // IDBに追加する処理
-      const promiseAddCategoryToIDB = categoryData => {
-        const data = {'id': categoryData.id, ...newCategoryData}
-
-        return this.$db.category.add(data)
-        .then(() => {
-          console.log('カテゴリ新規作成(IDB) >> 成功')
-        })
-      }
-      
-      if (this.$store.getters.isOnline) {
-        // オンラインの場合
-        // サーバーに追加する処理
-        const promiseAddCategoryToServer = this.$axios.post('/api/category/', sendData)
-        .then((response) => {
-          console.log('カテゴリ新規作成(API) >> 成功')
-          return response.data
-        })
-
-        promiseAddCategoryToServer
-        .then(categoryData => promiseAddCategoryToIDB(categoryData))
-        .then(() => {
-          // カテゴリデータの再読み込み
-          this.getCategoryDataFromDB()
-        })
-        .catch((e) => {
-          console.log('カテゴリ新規作成(Online) >> 失敗')
-          console.error(e.message)
-        })
-
-      } else {
-        // オフラインの場合
-        const newId = this.makeTmpId('category')
-        promiseAddCategoryToIDB({'id': newId})
-        .then(() => {
-          // offline_categoryに登録
-          return this.$db.offline_category.add({
-            'category_id': newId,
-            'type': 'create',
-            'data': sendData,
-          })
-        })
-        .then(() => {
-          this.errorMessage = 'カテゴリ新規作成の同期に失敗しました。オンラインに復帰後同期します。'
-          // カテゴリデータの再読み込み
-          this.getCategoryDataFromDB()
-        })
-        .catch((e) => {
-          console.log('カテゴリ新規作成(Offline) >> 失敗')
-          console.error(e.message)
-        })
-      }
+      this.$store.dispatch('addCategoryData', newCategoryData)
+      .then(() => this.$store.dispatch('replaceCategoryStateWithCategoryOnIDBCategory'))
     },
-    makeTmpId(type) {
-      // 【追加系api通信が新規IDを返すようになるまで使用】仮の新規IDを生成
-      let existIds = []
-      if (type == 'category') {
-        existIds = Object.keys(this.categoryData).map(id => Number(id))
-      } else if (type == 'task') {
-        existIds = this.tasks.map(task => task.task_id)
-      } else {
-        return
-      }
-
-      // 存在しているIDの最大値＋1
-      let maxId = null
-      if (existIds[0]) {
-        maxId = existIds.reduce((a, b) => {
-          return Math.max(a, b)
-        })
-        return maxId + 1
-      } else {
-        return 1
-      }
-    }
   }
 }
 </script>
