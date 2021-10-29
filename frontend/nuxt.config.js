68fabd31a65a880266fba38985c093005c90d7ec
Ry0xi <ryoki0525tnk@gmail.com>
2021-10-29 14:46:29 +0900

    [f-fix]コンフリクトの解消。

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -25,7 +25,11 @@ export default {
   css: ['@/assets/css/style.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/dexie.js', '~/plugins/network.js'],
+  plugins: [
+    '~/plugins/dexie.js',
+    '~/plugins/network.js',
+    '~/plugins/setup.js',
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
